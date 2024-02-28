47a62e9bf6d31a1b3c7acd5bb4930ad40ea3a646
AlexHerbertGit <alex.herbert.tech@gmail.com>
2024-02-28 14:06:41 +1300

    set up app.js file

---
+++ _student-app/app.js
@@ -0,0 +1,24 @@
+// Requires
+
+const http = requires("http");
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+
+//Init express application
+const app = express();
+
+//Define Entries as global array or storage
+const entries = [];
+app.locals.entries = entries;
+
+// Set the current directory to be views
+app.set("views", path.resolve(__dirname, "views"));
+
+//Define the render as ejs
+app.set("view engine", "ejs")
+
+//Body Parser to automatically set the HTML header type. 
+//Arguments ignore encoding the url extension.
+
+app.use(bodyParser.urlencoded({extended: false}))
